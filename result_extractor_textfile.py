cfae73e2e3268df41a59eaa2dbbf0dde68942313
jsonpanganiban <json.panganiban@gmail.com>
2016-12-30 14:15:51 +0800

    Update result_extractor_textfile.py

--- result_extractor_textfile.py
+++ result_extractor_textfile.py
@@ -1,4 +1,6 @@
-import time, sys, os, fnmatch, datetime
+import os
+import fnmatch
+import datetime
 from lxml import etree
 
 def save_test_result_to_text_file():
@@ -13,15 +15,12 @@ def save_test_result_to_text_file():
             tc_name_list.append(tc_name_attrib)
             tc_status_list.append(get_test_status_path(root, tc_name_attrib)[0].attrib['status'])
             tc_error_list.append(get_test_status_path(root, tc_name_attrib)[0].text)
-    range_length = len(tc_name_list)
 
     date_stamp = '{:%Y-%m-%d-%H%M%S}'.format(datetime.datetime.now())
     test_file_result = "test_result.txt" + "-" + str(date_stamp) + ".txt"
-    new_file = open(test_file_result, "w")
-    range_length = len(tc_name_list)
     with open(test_file_result, "a") as output_file:
-        for i in range(1, int(range_length) + 1):
-            output_file.write(tc_name_list[i - 1] + "\t" + tc_status_list[i - 1] + "\t" + tc_error_list[i - 1].encode('utf-8') + "\n")
+        for name, status, error in zip(tc_name_list, tc_status_list, tc_error_list):
+            output_file.write(name + "\t" + status + "\t" + error.encode('utf-8') + "\n")
     output_file.close()
 
 def get_test_status_path(root, tc_name_attrib):
@@ -36,17 +35,6 @@ def parse_xml_file(xml_file):
     return tree.getroot()
 
 def get_all_xml_files():
-    return traverse_thru_folders()
-
-def retrieve_from_current_folder():
-    xml_files = []
-    files_in_dir = listdir(getcwd())
-    for file_item in files_in_dir:
-        if file_item.endswith(".xml"):
-            xml_files.append(file_item)
-    return xml_files
-
-def traverse_thru_folders():
     xml_files = []
     for root, dirnames, filenames in os.walk('.'):
         for filename in fnmatch.filter(filenames, '*.xml'):
