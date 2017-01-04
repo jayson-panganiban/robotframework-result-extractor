886a6c09de6011dd29c385242bac630649eeffef
jsonpanganiban <json.panganiban@gmail.com>
2017-01-04 12:17:56 +0800

    Update result_extractor_excel.py

--- result_extractor_excel.py
+++ result_extractor_excel.py
@@ -4,11 +4,13 @@ import openpyxl
 import datetime
 from lxml import etree
 from openpyxl import Workbook
+from itertools import izip, count
 
 def save_test_result_to_excel_file():
     tc_name_list = []
     tc_status_list = []
     tc_error_list = []
+    tc_tag_list = []
     xml_files = get_all_xml_files()
     for each_xml_file in xml_files:
         root = parse_xml_file(each_xml_file)
@@ -17,18 +19,21 @@ def save_test_result_to_excel_file():
             tc_name_list.append(tc_name_attrib)
             tc_status_list.append(get_test_status_path(root, tc_name_attrib)[0].attrib['status'])
             tc_error_list.append(get_test_status_path(root, tc_name_attrib)[0].text)
+            tc_tag_list.append(str(get_test_tags(root, tc_name_attrib)))
 
     date_stamp = '{:%Y-%m-%d-%H%M%S}'.format(datetime.datetime.now())
     result_file = 'test_result-' + date_stamp + '.xlsx'
     wb = Workbook()
-    created_sheet = wb.create_sheet()
-    created_sheet['A1'] = 'Test Case Name'
-    created_sheet['B1'] = 'Test Case Status'
-    created_sheet['C1'] = 'Test Case Error'
-    for i, (name, status, error) in enumerate(zip(tc_name_list, tc_status_list, tc_error_list)):
-        created_sheet['A' + str(i + 2)] = name
-        created_sheet['B' + str(i + 2)] = status
-        created_sheet['C' + str(i + 2)] = error
+    worksheet = wb.worksheets[0]
+    worksheet['A1'] = 'Test Case Name'
+    worksheet['B1'] = 'Test Case Status'
+    worksheet['C1'] = 'Test Case Error'
+    worksheet['D1'] = 'Test Case Tags'
+    for i, name, status, error, tags in izip(count(), tc_name_list, tc_status_list, tc_error_list, tc_tag_list):
+        worksheet['A' + str(i + 2)] = name
+        worksheet['B' + str(i + 2)] = status
+        worksheet['C' + str(i + 2)] = error
+        worksheet['D' + str(i + 2)] = tags
     wb.save(filename=result_file)
 
 def get_test_status_path(root, tc_name_attrib):
@@ -37,6 +42,13 @@ def get_test_status_path(root, tc_name_attrib):
         return root.xpath(".//test[@name=" + tc_name_quoted + "]/status")
     else:
         return root.xpath(".//test[@name='" + tc_name_attrib + "']/status")
+
+def get_test_tags(root, tc_name_attrib):
+    if "'" in tc_name_attrib:
+        tc_name_quoted = '"%s"' % tc_name_attrib
+        return root.xpath(".//test[@name=" + tc_name_quoted + "]/tags/tag/text()")
+    else:
+        return root.xpath(".//test[@name='" + tc_name_attrib + "']/tags/tag/text()")
 
 def parse_xml_file(xml_file):
     tree = etree.parse(xml_file)
